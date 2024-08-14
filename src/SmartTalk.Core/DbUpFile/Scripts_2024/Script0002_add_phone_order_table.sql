dbb11f323dd0981fe441dae7e8ec95fa50a84bc9
Travis.C <1847254608@qq.com>
2024-08-14 16:54:44 +0800

    Add index

--- src/SmartTalk.Core/DbUpFile/Scripts_2024/Script0002_add_phone_order_table.sql
+++ src/SmartTalk.Core/DbUpFile/Scripts_2024/Script0002_add_phone_order_table.sql
@@ -30,3 +30,10 @@ create table if not exists `phone_order_order_item`
     `order_type` int not null,
     `created_date` datetime(3) not null
 ) charset = utf8mb4;
+
+CREATE INDEX `idx_id` ON `phone_order_record` (id);
+CREATE INDEX `idx_id` ON `phone_order_conversation` (id);
+CREATE INDEX `idx_id` ON `phone_order_order_item` (id);
+CREATE INDEX `idx_record_id` ON `phone_order_conversation` (record_id);
+CREATE INDEX `idx_record_id` ON `phone_order_order_item` (record_id);
+CREATE INDEX `idx_restuarant` ON `phone_order_record` (restaurant);
