80d7c7db1886e7b36d59e65c4db176eecb059707
zirui liu <maxon.liu@sjfood.com>
2024-08-16 18:44:12 +0800

    fix mapping

--- src/SmartTalk.Core/Mappings/UserAccountMapping.cs
+++ src/SmartTalk.Core/Mappings/UserAccountMapping.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using SmartTalk.Core.Domain.Account;
-using SmartTalk.Messages.Dto.Users;
+using SmartTalk.Messages.Dto.Account;
+using UserAccountDto = SmartTalk.Messages.Dto.Users.UserAccountDto;
 
 namespace SmartTalk.Core.Mappings;
 
@@ -9,5 +10,6 @@ public class UserAccountMapping : Profile
     public UserAccountMapping()
     {
         CreateMap<UserAccount, UserAccountDto>().ReverseMap();
+        CreateMap<UserAccountProfile, UserAccountProfileDto>().ReverseMap();
     }
 }
