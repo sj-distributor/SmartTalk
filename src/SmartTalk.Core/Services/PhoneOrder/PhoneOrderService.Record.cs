c0a04106c51184dbd4bf4f437ae0d0c855264a50
JOHNNY.X <2546088435@qq.com>
2024-08-28 15:53:59 +0800

    modify prompt

--- src/SmartTalk.Core/Services/PhoneOrder/PhoneOrderService.Record.cs
+++ src/SmartTalk.Core/Services/PhoneOrder/PhoneOrderService.Record.cs
@@ -67,27 +67,33 @@ public partial class PhoneOrderService
         
         await _phoneOrderDataProvider.AddPhoneOrderRecordsAsync(orderRecord, cancellationToken: cancellationToken).ConfigureAwait(false);
 
-        _backgroundJobClient.Enqueue(() => ExtractAiOrderInformationAsync(transcription, orderRecord[0].Id, false, cancellationToken));
+        _backgroundJobClient.Enqueue(() => ExtractAiOrderInformationAsync(transcription, orderRecord[0].Id, cancellationToken));
         
         if (!string.IsNullOrEmpty(recordInfo.WorkWeChatRobotUrl))
             await SendWorkWeChatRobotNotifyAsync(command.RecordContent, recordInfo, transcription, cancellationToken).ConfigureAwait(false);
     }
     
-    public async Task ExtractAiOrderInformationAsync(string transcription, int recordId, bool isUseGpt4, CancellationToken cancellationToken)
+    public async Task ExtractAiOrderInformationAsync(string transcription, int recordId, CancellationToken cancellationToken)
     {
-        if (!await DecideWhetherPlaceAnOrderAsync(transcription, false, cancellationToken).ConfigureAwait(false)) return;
+        if (!await DecideWhetherPlaceAnOrderAsync(transcription, cancellationToken).ConfigureAwait(false)) return;
         
-        await ExtractMenuAsync(transcription, false, recordId, cancellationToken).ConfigureAwait(false);
+        await ExtractMenuAsync(transcription, recordId, cancellationToken).ConfigureAwait(false);
     }
 
-    public async Task ExtractMenuAsync(string transcription, bool isUseGpt4, int recordId, CancellationToken cancellationToken)
+    public async Task ExtractMenuAsync(string transcription, int recordId, CancellationToken cancellationToken)
     {
         var messages = new List<CompletionsRequestMessageDto>
         {
             new ()
             {
                 Role = "system",
-                Content = new CompletionsStringContent("\"You are a highly skilled AI capable of understanding and extracting dish information from text. Your task is to analyze the following article and find out the name of the dish, corresponding quantity, and price. Translate all dish names into traditional Chinese. Do not repeat the name of the dish. If the quantity is not mentioned, assume it is one. If no price is provided, the price is assumed to be zero. Ignore any total price or total amount that relates to the entire order rather than individual items. For example, if the article mentions \"Barbecue pork chow fun, spicy pork chops, total $44.90,\" you should extract:\n[{\"food_name\": \"Char Siu Fried noodles\", \"quantity\": 1, \"price\": 0},\n{\"food_name\": \"Spicy pork chop\", \"quantity\": 1, \"price\": 0}]")
+                Content = new CompletionsStringContent("你是一个精通餐厅话术的客服经理,专门识别与提取电话录音中的菜单。\n" +
+                                                       "你需要根据电话录音的内容，判断客人通话中需要今天内出单给厨房配餐的菜单中的菜品名和数量以及单价\n" +
+                                                       "注意用规定的格式返回:规则: [{\"food_name\":\"菜品名\",\"quantity\":1,\"price\":0}]"+
+                                                       "具体有以下规则:\n1.对话中有出现过菜品名、菜品价格、菜品数量等词语\n2.如果对话中没有出现菜品价格和菜品数量，则菜品价格为0菜品数量为1\n3.禁止将总价赋予某个菜品，总价一般是最后的价格\n4.严格按着规定格式返回，不要添加```json```\n5.不要提取重复的菜名或者相识的菜品\n6.提取出来的菜名翻译成中文\n" +
+                                                       "- 样本与输出：\n" +
+                                                       "反面例子：\"input:你好 你好 我想要一份松花鱼 红烧松花鱼还是糖醋松花鱼 要红烧松花鱼 好还有需要的吗 上海青有吗 有上海青 多少钱 27 好再来两份小炒肉 青椒小炒肉还是黄牛小炒肉 青椒小炒肉 好359 大概要多少 三十五分钟 还要一个六块的香肠 好.output:[{\"food_name\":\"糖醋松花鱼\",\"quantity\":1,\"price\":0},{\"food_name\":\"松花鱼\",\"quantity\":1,\"price\":0},{\"food_name\":\"红烧松花鱼\",\"quantity\":1,\"price\":0},{\"food_name\":\"黄牛小炒肉\",\"quantity\":2,\"price\":0},{\"food_name\":\"青椒小炒肉\",\"quantity\":2,\"price\":359},{\"food_name\":\"小炒肉\",\"quantity\":2,\"price\":0},{\"food_name\":\"上海青\",\"quantity\":1,\"price\":27}],{\"food_name\":\"上海青\",\"quantity\":1,\"price\":27},{\"food_name\":\"六块的香肠\",\"quantity\":1,\"price\":0}]" +
+                                                       "正面例子：\"input:你好 你好 我想要一份松花鱼 红烧松花鱼还是糖醋松花鱼 要红烧松花鱼 好还有需要的吗 上海青有吗 有上海青 多少钱 27 好再来两份小炒肉 青椒小炒肉还是黄牛小炒肉 青椒小炒肉 好359 大概要多少 三十五分钟 还要一个六块的香肠 好.output:[{\"food_name\":\"红烧松花鱼\",\"quantity\":1,\"price\":0},{\"food_name\":\"青椒小炒肉\",\"quantity\":2,\"price\":0},{\"food_name\":\"上海青\",\"quantity\":1,\"price\":27},{\"food_name\":\"香肠\",\"quantity\":1,\"price\":6}]")
             },
             new ()
             {
@@ -98,7 +104,7 @@ public partial class PhoneOrderService
 
         var gptResponse = await _smartiesClient.PerformQueryAsync(new AskGptRequest
         {
-            Model = isUseGpt4 ? OpenAiModel.Gpt4o : OpenAiModel.Gpt35Turbo16K,
+            Model = OpenAiModel.Gpt4o,
             Messages = messages.Select(m => new CompletionsRequestMessageDto
             {
                 Content = m.Content,
@@ -119,25 +125,30 @@ public partial class PhoneOrderService
         await _phoneOrderDataProvider.AddPhoneOrderItemAsync(dishes, cancellationToken: cancellationToken).ConfigureAwait(false);
     }
 
-    public async Task<bool> DecideWhetherPlaceAnOrderAsync(string transcription, bool isUseGpt4, CancellationToken cancellationToken)
+    public async Task<bool> DecideWhetherPlaceAnOrderAsync(string transcription, CancellationToken cancellationToken)
     {
         var messages = new List<CompletionsRequestMessageDto>
         {
             new ()
             {
                 Role = "system",
-                Content = new CompletionsStringContent("You are a highly skilled AI trained to understand language and determine if there is a need to place an order. Your task is to read the following article, analyze whether there is a demand for an order, and clearly determine whether the order is a takeaway (for example, if it mentions keywords such as \"takeaway\", \"delivery\", \"how long\", etc.) rather than a reservation. The purpose is to ensure that if there is an order demand and it is a takeaway, the order is placed successfully. If the article indicates a need for takeout, return yes, if not, return no.Use the following template to generate traditional Chinese:result: yes or no")
+                Content = new CompletionsStringContent("你是一个精通餐厅话术的客服经理,专门分析电话录音中是否有下单动作。\n" +
+                                                       "你需要根据电话录音的内容，判断客人通话的目的是否为【叫外卖】、【预订餐】等需要今天内出单给厨房配餐\n" +
+                                                       "注意返回格式: true or false\n"+
+                                                       "具体有以下特征：\n正面特征:\n1.对话中有出现过菜品名、菜品价格、菜品数量、电话号码、总价格等词语\n2.有提及需要多久\n反面特征:\n1.没有出现菜品名、菜品价格、菜品数量、电话号码、总价格等词语\n2.出现预定、询问订单的情况" +
+                                                       "反面例子：\"你好 你好 星期五可以预定吗 可以 你需要什么 有什么套餐吗 有宴会套餐a1488和宴会套餐b1688 我需要宴会套餐a 好的 你的手机号码 xxx-xxx-xxx 好的.\" false;\n" +
+                                                       "正面例子：\"你好 你好 我想要一份松花鱼 好还需要什么 上海青有吗 有 好再来两份小炒肉 好的 大概什么时候好 二十分钟之后 好.\"true。")
             },
             new ()
             {
                 Role = "user",
-                Content = new CompletionsStringContent($" minutes:\n\"{transcription}\"\nresult:\n")
+                Content = new CompletionsStringContent($"article:\n\"{transcription}\"\nresult:")
             }
         };
 
         var gptResponse = await _smartiesClient.PerformQueryAsync(new AskGptRequest
         {
-            Model = isUseGpt4 ? OpenAiModel.Gpt4o : OpenAiModel.Gpt35Turbo16K,
+            Model = OpenAiModel.Gpt4o,
             Messages = messages.Select(m => new CompletionsRequestMessageDto
             {
                 Content = m.Content,
@@ -147,12 +158,7 @@ public partial class PhoneOrderService
 
         Log.Information("Transcription: {transcription},\n result: {result}", transcription, gptResponse.Data.Response);
 
-        return gptResponse.Data.Response switch
-        {
-            "result: no" => false,
-            "result: yes" => true,
-            _ => false
-        };
+        return bool.Parse(gptResponse.Data.Response);
     }
 
     private async Task<bool> CheckPhoneOrderRecordDurationAsync(byte[] recordContent, CancellationToken cancellationToken)
