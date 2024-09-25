de22a283470e01c8ea80280dab455c7672fe810c
JOHNNY.X <2546088435@qq.com>
2024-09-25 18:01:34 +0800

    Update PhoneOrderService.Record.cs

--- src/SmartTalk.Core/Services/PhoneOrder/PhoneOrderService.Record.cs
+++ src/SmartTalk.Core/Services/PhoneOrder/PhoneOrderService.Record.cs
@@ -118,6 +118,7 @@ public partial class PhoneOrderService
         
         var (goalTexts, shoppingCart, conversations) = await PhoneOrderTranscriptionAsync(phoneOrderInfo, record, audioContent, cancellationToken).ConfigureAwait(false);
         
+        record.Tips = goalTexts.First().Split([':'], 2)[1].Trim();
         record.Status = PhoneOrderRecordStatus.Sent;
         record.TranscriptionText = string.Join("\n", goalTexts);
         conversations.Where(x => string.IsNullOrEmpty(x.Answer)).ForEach(x => x.Answer = string.Empty);
