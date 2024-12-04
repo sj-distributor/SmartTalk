9627ba58b9d6bf88f2f48d30b54243cd691feb1c
JOHNNY.X <2546088435@qq.com>
2024-12-05 07:11:40 +0800

    Update Script0015_add_user_role_permission.sql

--- src/SmartTalk.Core/DbUpFile/Scripts_2024/Script0015_add_user_role_permission.sql
+++ src/SmartTalk.Core/DbUpFile/Scripts_2024/Script0015_add_user_role_permission.sql
@@ -31,7 +31,7 @@ WHERE r.name = 'SuperAdministrator';
 
 INSERT INTO role_permission (created_date, last_modified_date, role_id, permission_id)
 SELECT NOW(3), NOW(3), r.id, p.id
-FROM role r JOIN permission p ON p.name IN ('CanViewPhoneOrder','CanViewAccountManagement','CanCopyAccount','CanCreateAccount','CanDeleteAccount')
+FROM role r JOIN permission p ON p.name IN ('CanViewPhoneOrder','CanViewAccountManagement','CanCopyAccount','CanCreateAccount','CanDeleteAccount','CanUpdateAccount')
 WHERE r.name = 'Administrator';
 
 INSERT INTO role_permission (created_date, last_modified_date, role_id, permission_id)
