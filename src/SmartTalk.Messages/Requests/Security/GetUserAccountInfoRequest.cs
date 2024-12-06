9c4b14ecd1e4d8ce5f8d2f65bc6ce5c690269f48
JOHNNY.X <2546088435@qq.com>
2024-12-06 08:26:26 +0800

    Update

--- src/SmartTalk.Messages/Requests/Security/GetUserAccountInfoRequest.cs
+++ src/SmartTalk.Messages/Requests/Security/GetUserAccountInfoRequest.cs
@@ -1,10 +1,10 @@
 using Mediator.Net.Contracts;
-using Smarties.Messages.Attributes;
+using SmartTalk.Messages.Attributes;
 using SmartTalk.Messages.Responses;
 
 namespace SmartTalk.Messages.Requests.Security;
 
-[SmartiesAuthorize("CanCopyAccount")]
+[SmartTalkAuthorize("CanCopyAccount")]
 public class GetUserAccountInfoRequest : IRequest
 {
     public int UserId { get; set; }
