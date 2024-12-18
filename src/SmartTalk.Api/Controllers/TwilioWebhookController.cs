a6e3fc640894c0ed2b54758c57a29b5232c894d6
JOHNNY.X <2546088435@qq.com>
2024-12-18 18:56:20 +0800

    Update TwilioWebhookController.cs

--- src/SmartTalk.Api/Controllers/TwilioWebhookController.cs
+++ src/SmartTalk.Api/Controllers/TwilioWebhookController.cs
@@ -1,11 +1,7 @@
-using Serilog;
 using Mediator.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.WebUtilities;
 using SmartTalk.Messages.Commands.Twilio;
-using SmartTalk.Messages.DTO.Communication.Twilio;
-using SmartTalk.Messages.Enums.Communication.PhoneCall;
 
 namespace SmartTalk.Api.Controllers;
 
@@ -21,31 +17,10 @@ public class TwilioWebhookController : ControllerBase
         _mediator = mediator;
     }
     
-    
     [Route("phonecall"), HttpPost]
-    public async Task<IActionResult> HandlePhoneCallStatusCallBackAsync()
+    public async Task<IActionResult> HandlePhoneCallStatusCallBackAsync([FromBody] HandlePhoneCallStatusCallBackCommand command)
     {
-        using (var reader = new StreamReader(Request.Body))
-        {
-            var body = await reader.ReadToEndAsync();
-            
-            var query = QueryHelpers.ParseQuery(body);
-            
-            Log.Information("Receiving Phone call status callback command, from: {from} 、to: {to} 、status: {status}", query["From"], query["To"], query["CallStatus"]);
-            
-            await _mediator.SendAsync(new HandlePhoneCallStatusCallBackCommand
-            {
-                Provider = PhoneCallProvider.Twilio,
-                CallBackMessage = new TwilioPhoneCallStatusCallbackDto
-                {
-                    To = query["To"],
-                    From = query["From"],
-                    CallSid = query["CallSid"],
-                    Status = query["CallStatus"],
-                    Direction = query["Direction"]
-                }
-            }).ConfigureAwait(false);
-        }
+        await _mediator.SendAsync(command).ConfigureAwait(false);
         
         return Ok();
     }
