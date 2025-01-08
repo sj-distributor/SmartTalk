b067ab537c45430b72e6b9b94a9ba4b77b6da379
MonesyH <monesy.huang@sjfood.com>
2025-01-08 23:33:45 +0800

    Update Script0021_add_ai_speech_assistant_tables.sql

--- src/SmartTalk.Core/DbUpFile/Scripts_2024/Script0021_add_ai_speech_assistant_tables.sql
+++ src/SmartTalk.Core/DbUpFile/Scripts_2024/Script0021_add_ai_speech_assistant_tables.sql
@@ -23,3 +23,8 @@ create table if not exists `ai_speech_assistant_user_profile`
     `profile_json` text null,
     `created_date` datetime(3) not null
 ) charset = utf8mb4;
+
+CREATE INDEX idx_did_number ON ai_speech_assistant (did_number);
+CREATE INDEX idx_assistant_id ON ai_speech_assistant_prompt_template (assistant_id);
+CREATE INDEX idx_caller_number ON ai_speech_assistant_user_profile (caller_number);
+CREATE INDEX idx_assistant_id ON ai_speech_assistant_user_profile (assistant_id);
