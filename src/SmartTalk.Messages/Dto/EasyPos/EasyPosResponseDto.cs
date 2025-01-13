e692cf2ea690c55ef19fbd7ea8741f2ef15d2a6d
LIZZIE.L <lyidi444@gmail.com>
2025-01-13 09:42:09 +0800

    Update EasyPosResponseDto.cs

--- src/SmartTalk.Messages/Dto/EasyPos/EasyPosResponseDto.cs
+++ src/SmartTalk.Messages/Dto/EasyPos/EasyPosResponseDto.cs
@@ -36,6 +36,9 @@ public class EasyPosResponseProduct
 
     [JsonProperty("localizations")]
     public List<EasyPosResponseLocalization> Localizations { get; set; }
+    
+    [JsonProperty("modifierGroups")]
+    public List<EasyPosResponsemodifierGroups> ModifierGroups { get; set; }
 }
 
 public class EasyPosResponseLocalization
@@ -50,3 +53,43 @@ public class EasyPosResponseLocalization
     public string Value { get; set; }
 }
 
+public class EasyPosResponsemodifierGroups
+{
+    [JsonProperty("id")]
+    public long Id { get; set; }
+
+    [JsonProperty("companyId")]
+    public long CompanyId { get; set; }
+
+    [JsonProperty("merchantId")]
+    public long MerchantId { get; set; }
+
+    [JsonProperty("productId")]
+    public long ProductId { get; set; }
+    
+    [JsonProperty("minimumSelect")]
+    public int MinimumSelect { get; set; }
+    
+    [JsonProperty("maximumSelect")]
+    public int MaximumSelect { get; set; }
+    
+    [JsonProperty("maximumRepetition")]
+    public int MaximumRepetition { get; set; }
+    
+    [JsonProperty("desc")]
+    public string Desc { get; set; }
+    
+    [JsonProperty("sort")]
+    public int Sort { get; set; }
+    
+    [JsonProperty("localizations")]
+    public List<EasyPosResponseLocalization> Localizations { get; set; }
+    
+    [JsonProperty("modifierProducts")]
+    public List<EasyPosResponseModifierProducts> ModifierProducts { get; set; }
+}
+
+public class EasyPosResponseModifierProducts
+{
+    
+}
