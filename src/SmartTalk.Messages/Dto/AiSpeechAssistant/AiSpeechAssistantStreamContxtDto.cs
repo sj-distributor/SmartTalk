724f2d65033a9edcb35b6bf95780a584904283b6
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-01-16 14:58:27 +0800

    Enhance ai speech context

--- src/SmartTalk.Messages/Dto/AiSpeechAssistant/AiSpeechAssistantStreamContxtDto.cs
+++ src/SmartTalk.Messages/Dto/AiSpeechAssistant/AiSpeechAssistantStreamContxtDto.cs
@@ -1,3 +1,5 @@
+using SmartTalk.Messages.Enums.AiSpeechAssistant;
+
 namespace SmartTalk.Messages.Dto.AiSpeechAssistant;
 
 public class AiSpeechAssistantStreamContxtDto
@@ -15,4 +17,32 @@ public class AiSpeechAssistantStreamContxtDto
     public bool InitialConversationSent { get; set; } = false;
 
     public bool ShowTimingMath { get; set; } = false;
+    
+    public AiSpeechAssistantStreamContxtUserInfoDto UserInfo { get; set; }
+
+    public List<AiSpeechAssistantStreamContxtOrderItemDto> OrderItems { get; set; } = [];
+}
+
+public class AiSpeechAssistantStreamContxtUserInfoDto
+{
+    public string UserName { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public string Address { get; set; }
+    
+    public string DeliveryTime { get; set; }
+    
+    public AiSpeechAssistantDeliverType DeliveryType { get; set; }
+}
+
+public class AiSpeechAssistantStreamContxtOrderItemDto
+{
+    public string Name { get; set; }
+    
+    public decimal Price { get; set; }
+    
+    public int Quantity { get; set; }
+    
+    public string Comment { get; set; }
 }
