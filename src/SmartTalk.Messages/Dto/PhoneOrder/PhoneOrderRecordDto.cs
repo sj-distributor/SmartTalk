39a8edf5016199b04f922071cd4e9cd157046d36
157 <serena.xiao@sjfood.com>
2025-01-22 10:04:11 +0800

    Update code

--- src/SmartTalk.Messages/Dto/PhoneOrder/PhoneOrderRecordDto.cs
+++ src/SmartTalk.Messages/Dto/PhoneOrder/PhoneOrderRecordDto.cs
@@ -25,8 +25,6 @@ public class PhoneOrderRecordDto
 
     public DateTimeOffset CreatedDate { get; set; }
     
-    public UserAccountDto UserAccount { get; set; }
-    
     public string TranscriptionJobId { get; set; }
     
     public string LastModifiedByName { get; set; }
