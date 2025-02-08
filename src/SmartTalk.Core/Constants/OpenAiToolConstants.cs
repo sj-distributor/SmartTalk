1a2ac8454a7bc78be7f061e87012d9ee642b6e0e
MonesyH <monesy.huang@sjfood.com>
2025-02-08 09:05:35 +0800

    Update OpenAiToolConstants.cs

--- src/SmartTalk.Core/Constants/OpenAiToolConstants.cs
+++ src/SmartTalk.Core/Constants/OpenAiToolConstants.cs
@@ -3,10 +3,11 @@ namespace SmartTalk.Core.Constants;
 public static class OpenAiToolConstants
 {
     // Ai speech assistant tools
+    public const string Hangup = "hangup";
+    public const string TransferCall = "transfer_call";
     public const string RepeatOrder = "repeat_order";
     public const string UpdateOrder = "update_order";
     public const string ConfirmOrder = "order";
-    public const string TransferCall = "transfer_call";
     public const string HandlePhoneOrderIssues = "handle_phone_order_issues";
     public const string HandleThirdPartyDelayedDelivery = "handle_third_party_delayed_delivery";
     public const string HandleThirdPartyPickupTimeChange = "handle_third_party_pickup_time_change";
@@ -15,5 +16,4 @@ public static class OpenAiToolConstants
     public const string HandlePromotionCalls = "handle_promotion_calls";
     public const string CheckOrderStatus = "check_order_status";
     public const string RequestOrderDelivery = "request_order_delivery";
-    public const string Hangup = "hangup";
 }
