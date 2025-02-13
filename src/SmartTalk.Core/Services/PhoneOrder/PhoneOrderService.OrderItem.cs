fa682500c7a0304f45fd0786d821308fd766b30a
MonesyH <monesy.huang@sjfood.com>
2025-02-14 07:19:28 +0800

    Update PhoneOrderService.OrderItem.cs

--- src/SmartTalk.Core/Services/PhoneOrder/PhoneOrderService.OrderItem.cs
+++ src/SmartTalk.Core/Services/PhoneOrder/PhoneOrderService.OrderItem.cs
@@ -59,7 +59,7 @@ public partial class PhoneOrderService
 
         var request = new PlaceOrderToEasyPosRequestDto
         {
-            Type = 9,
+            Type = 1,
             IsTaxFree = true,
             Notes = string.Empty,
             OrderItems = orderItems.Select(x => new PhoneCallOrderItem
