aac7c641b6ff2e5deafe8102d76dce9db18e4507
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-03-06 16:37:09 +0800

    Update AiSpeechAssistantService.cs

--- src/SmartTalk.Core/Services/AiSpeechAssistant/AiSpeechAssistantService.cs
+++ src/SmartTalk.Core/Services/AiSpeechAssistant/AiSpeechAssistantService.cs
@@ -172,7 +172,7 @@ public class AiSpeechAssistantService : IAiSpeechAssistantService
     private async Task<(Domain.AISpeechAssistant.AiSpeechAssistant Assistant, string Prompt)> BuildingAiSpeechAssistantKnowledgeBaseAsync(string from, string to, int? assistantId, CancellationToken cancellationToken)
     {
         var (assistant, promptTemplate, userProfile) = await _aiSpeechAssistantDataProvider
-            .GetAiSpeechAssistantInfoByNumbersAsync(from, to, cancellationToken).ConfigureAwait(false);
+            .GetAiSpeechAssistantInfoByNumbersAsync(from, to, assistantId, cancellationToken).ConfigureAwait(false);
         
         Log.Information("Matching Ai speech assistant: {@Assistant}、{@PromptTemplate}、{@UserProfile}", assistant, promptTemplate, userProfile);
 
