0adaad0418b8fa403c56a0bbd24f9be93bb292bc
MonesyH <monesy.huang@sjfood.com>
2025-03-17 12:19:52 +0800

    Update AiSpeechAssistantService.cs

--- src/SmartTalk.Core/Services/AiSpeechAssistant/AiSpeechAssistantService.cs
+++ src/SmartTalk.Core/Services/AiSpeechAssistant/AiSpeechAssistantService.cs
@@ -319,7 +319,7 @@ public class AiSpeechAssistantService : IAiSpeechAssistantService
     private async Task SendToTwilioAsync(WebSocket twilioWebSocket, WebSocket openAiWebSocket, AiSpeechAssistantStreamContextDto context, CancellationToken cancellationToken)
     {
         Log.Information("Receive buffer from openai, and Sending to twilio. The buffer length: 10");
-        var buffer = new byte[1024 * 10];
+        var buffer = new byte[1024 * 20];
         try
         {
             while (openAiWebSocket.State == WebSocketState.Open)
