702423f5516b921d01b9dbe83235c141cc1ff83c
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-03-24 17:08:30 +0800

    rename

--- src/SmartTalk.Core/Handlers/RequestHandlers/AiSpeechAssistant/GetAssistantNumberRequestHandler.cs
+++ src/SmartTalk.Core/Handlers/RequestHandlers/AiSpeechAssistant/GetAssistantNumberRequestHandler.cs
@@ -5,7 +5,7 @@ using SmartTalk.Messages.Requests.AiSpeechAssistant;
 
 namespace SmartTalk.Core.Handlers.RequestHandlers.AiSpeechAssistant;
 
-public class GetAssistantNumberRequestHandler : IRequestHandler<GetAssistantByIdRequest, GetAssistantByIdResponse>
+public class GetAssistantNumberRequestHandler : IRequestHandler<GetAiSpeechAssistantByIdRequest, GetAiSpeechAssistantByIdResponse>
 {
     private readonly IAiSpeechAssistantService _assistantService;
 
@@ -14,8 +14,8 @@ public class GetAssistantNumberRequestHandler : IRequestHandler<GetAssistantById
         _assistantService = assistantService;
     }
 
-    public async Task<GetAssistantByIdResponse> Handle(IReceiveContext<GetAssistantByIdRequest> context, CancellationToken cancellationToken)
+    public async Task<GetAiSpeechAssistantByIdResponse> Handle(IReceiveContext<GetAiSpeechAssistantByIdRequest> context, CancellationToken cancellationToken)
     {
-        return await _assistantService.GetAssistantNumberAsync(context.Message, cancellationToken).ConfigureAwait(false);
+        return await _assistantService.GetAiSpeechAssistantByIdAsync(context.Message, cancellationToken).ConfigureAwait(false);
     }
 }
