2ca0ba8fec52aaf63fe5bcb98f24418e5c9082e0
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-04-08 15:14:58 +0800

    Update OpenAiRealtimeSessionsInitialRequestDto.cs

--- src/SmartTalk.Messages/Dto/OpenAi/OpenAiRealtimeSessionsInitialRequestDto.cs
+++ src/SmartTalk.Messages/Dto/OpenAi/OpenAiRealtimeSessionsInitialRequestDto.cs
@@ -7,14 +7,8 @@ public class OpenAiRealtimeSessionsInitialRequestDto
     [JsonProperty("model")]
     public string Model { get; set; }
     
-    [JsonProperty("turn_detection", NullValueHandling = NullValueHandling.Ignore)]
+    [JsonProperty("turn_detection")]
     public object TurnDetection { get; set; }
-
-    [JsonProperty("input_audio_format", NullValueHandling = NullValueHandling.Ignore)]
-    public string InputAudioFormat { get; set; }
-    
-    [JsonProperty("output_audio_format", NullValueHandling = NullValueHandling.Ignore)]
-    public string OutputAudioFormat { get; set; }
 
     [JsonProperty("voice")]
     public string Voice { get; set; }
@@ -22,15 +16,12 @@ public class OpenAiRealtimeSessionsInitialRequestDto
     [JsonProperty("instructions")]
     public string Instructions { get; set; }
 
-    [JsonProperty("modalities", NullValueHandling = NullValueHandling.Ignore)]
+    [JsonProperty("modalities")]
     public List<string> Modalities { get; set; }
-
-    [JsonProperty("temperature", NullValueHandling = NullValueHandling.Ignore)]
-    public double Temperature { get; set; }
 
     [JsonProperty("input_audio_transcription")]
     public object InputAudioTranscription { get; set; }
 
-    [JsonProperty("tools", NullValueHandling = NullValueHandling.Ignore)]
+    [JsonProperty("tools")]
     public List<object> Tools { get; set; }
 }
