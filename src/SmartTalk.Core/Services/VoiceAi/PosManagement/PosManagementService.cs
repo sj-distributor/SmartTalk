f6cdb0344521ecf814e17037261f2e0d28c85f23
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-04-22 10:20:53 +0800

    Create Voice Pos Module

---
+++ src/SmartTalk.Core/Services/VoiceAi/PosManagement/PosManagementService.cs
@@ -0,0 +1,11 @@
+using SmartTalk.Core.Ioc;
+
+namespace SmartTalk.Core.Services.VoiceAi.PosManagement;
+
+public interface IPosManagementService : IScopedDependency
+{
+}
+
+public class PosManagementService : IPosManagementService
+{
+}
