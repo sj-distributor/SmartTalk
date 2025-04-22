1ec19aece75b697ad86e281ec619bc4409606590
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-04-22 10:34:28 +0800

    Initial pos management module

---
+++ src/SmartTalk.Messages/Dto/VoiceAi/PosManagement/PosCompanyDto.cs
@@ -0,0 +1,22 @@
+namespace SmartTalk.Messages.Dto.VoiceAi.PosManagement;
+
+public class PosCompanyDto
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    public string Address { get; set; }
+    
+    public bool Status { get; set; }
+    
+    public int CreatedBy { get; set; }
+    
+    public DateTimeOffset CreatedDate { get; set; }
+    
+    public int? LastModifiedBy { get; set; }
+    
+    public DateTimeOffset? LastModifiedDate { get; set; }
+}
