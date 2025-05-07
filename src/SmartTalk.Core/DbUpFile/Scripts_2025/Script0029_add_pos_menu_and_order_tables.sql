6d891571be74b417d71d05842821f87fd3d531a1
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-05-07 15:45:55 +0800

    Update Script0029_add_pos_menu_and_order_tables.sql

--- src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0029_add_pos_menu_and_order_tables.sql
+++ src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0029_add_pos_menu_and_order_tables.sql
@@ -67,11 +67,11 @@ create table if not exists `pos_period`
 (
     `id` int primary key auto_increment,
     `store_id` int not null,
+    `period_id` varchar(36) not null,
     `name` varchar(64) not null,
     `day_of_weeks` varchar(16) not null,
     `start_time` time not null,
     `end_time` time not null,
-    `type` int not null,
     `created_by` int null,
     `created_date` datetime(3) not null,
     `last_modified_by` int null,
