f78b59a90ed3e4a21317fa9cd4866a670129c0d4
MonesyH <MonesyH@users.noreply.github.com>
2025-05-09 16:11:28 +0800

    Temp

---
+++ src/SmartTalk.Core/Services/RealtimeAi/wss/IRealtimeAiSWssClient.cs
@@ -0,0 +1,17 @@
+using System.Net.WebSockets;
+
+namespace SmartTalk.Core.Services.RealtimeAi.wss;
+
+public interface IRealtimeAiWssClient : IAsyncDisposable
+{
+    WebSocketState CurrentState { get; }
+    Uri EndpointUri { get; }
+
+    event Func<string, Task> MessageReceivedAsync;
+    event Func<WebSocketState, string, Task> StateChangedAsync;
+    event Func<Exception, Task> ErrorOccurredAsync;
+
+    Task ConnectAsync(Uri endpointUri, Dictionary<string, string> customHeaders, CancellationToken cancellationToken);
+    Task SendMessageAsync(string message, CancellationToken cancellationToken);
+    Task DisconnectAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken);
+}
