94a8711a52019de4cab2d1b879061ad3d00d08c4
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-05-22 18:29:14 +0800

    Update TwilioUtilService.cs

--- src/SmartTalk.Core/Services/RealtimeAi/Services/TwilioUtilService.cs
+++ src/SmartTalk.Core/Services/RealtimeAi/Services/TwilioUtilService.cs
@@ -242,7 +242,6 @@ public class TwilioUtilUtilService : ITwilioUtilService
         {
             _aiEngine.SessionStatusChangedAsync -= OnAiSessionStatusChangedAsync;
             _aiEngine.AiAudioOutputReadyAsync -= OnAiAudioOutputReadyAsync;
-            _aiEngine.TranscriptionReadyAsync -= OnAiTranscriptionReadyAsync;
             _aiEngine.ErrorOccurredAsync -= OnAiErrorOccurredAsync;
         }
     }
