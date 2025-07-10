ccf4cf64f5439943260ab6f207d0fe6166b2cd54
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-07-10 14:04:49 +0800

    Add assistant number match

---
+++ src/SmartTalk.Core/Domain/AISpeechAssistant/AssistantNumberMatch.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartTalk.Core.Domain.AISpeechAssistant;
+
+[Table("assistant_number_match")]
+public class AssistantNumberMatch : IEntity, IHasCreatedFields
+{
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Column("assistant_id")]
+    public int AssistantId { get; set; }
+
+    [Column("from"), StringLength(48)]
+    public string From { get; set; }
+
+    [Column("to"), StringLength(48)]
+    public string To { get; set; }
+
+    [Column("created_date")]
+    public DateTimeOffset CreatedDate { get; set; }
+}
