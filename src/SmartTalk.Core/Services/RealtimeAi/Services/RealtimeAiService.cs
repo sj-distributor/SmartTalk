f915c0d01566ec9310b48e683bb1c6302aaa3131
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-07-14 14:46:49 +0800

    update

--- src/SmartTalk.Core/Services/RealtimeAi/Services/RealtimeAiService.cs
+++ src/SmartTalk.Core/Services/RealtimeAi/Services/RealtimeAiService.cs
@@ -17,6 +17,7 @@ using SmartTalk.Core.Services.RealtimeAi.Adapters;
 using SmartTalk.Messages.Commands.Attachments;
 using SmartTalk.Messages.Commands.RealtimeAi;
 using SmartTalk.Messages.Dto.Attachments;
+using SmartTalk.Messages.Dto.Smarties;
 using JsonException = System.Text.Json.JsonException;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
@@ -84,7 +85,9 @@ public class RealtimeAiService : IRealtimeAiService
         
         BuildConversationEngine(assistant.ModelProvider);
         
-        await _conversationEngine.StartSessionAsync(assistant, initialPrompt, inputFormat, outputFormat, cancellationToken).ConfigureAwait(false);
+        var finalPrompt = await BuildingAiSpeechAssistantKnowledgeBaseAsync(assistant, cancellationToken).ConfigureAwait(false);
+        
+        await _conversationEngine.StartSessionAsync(assistant, string.IsNullOrWhiteSpace(finalPrompt) ? initialPrompt : finalPrompt, inputFormat, outputFormat, cancellationToken).ConfigureAwait(false);
         
         await ReceiveFromWebSocketClientAsync(
             new RealtimeAiEngineContext { AgentId = assistant.AgentId, InitialPrompt = initialPrompt, InputFormat = inputFormat, OutputFormat = outputFormat }, cancellationToken).ConfigureAwait(false);
@@ -95,10 +98,83 @@ public class RealtimeAiService : IRealtimeAiService
         var client = _realtimeAiSwitcher.WssClient(provider);
         var adapter = _realtimeAiSwitcher.ProviderAdapter(provider);
         
-        _conversationEngine = new RealtimeAiConversationEngine(_smartiesClient, adapter, client, _aiSpeechAssistantDataProvider);
+        _conversationEngine = new RealtimeAiConversationEngine(adapter, client);
         _conversationEngine.AiAudioOutputReadyAsync += OnAiAudioOutputReadyAsync;
         _conversationEngine.AiDetectedUserSpeechAsync += OnAiDetectedUserSpeechAsync;
         _conversationEngine.AiTurnCompletedAsync += OnAiTurnCompletedAsync;
+    }
+    
+    private async Task<string> BuildingAiSpeechAssistantKnowledgeBaseAsync(Domain.AISpeechAssistant.AiSpeechAssistant assistant, CancellationToken cancellationToken)
+    {
+        if (assistant?.Knowledge == null || string.IsNullOrEmpty(assistant.Knowledge?.Prompt)) return string.Empty;
+
+        var pstTime = TimeZoneInfo.ConvertTime(DateTimeOffset.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time"));
+        var currentTime = pstTime.ToString("yyyy-MM-dd HH:mm:ss");
+        
+        var finalPrompt = assistant.Knowledge.Prompt
+            .Replace("#{current_time}", currentTime)
+            .Replace("#{pst_date}", $"{pstTime.Date:yyyy-MM-dd} {pstTime.DayOfWeek}");
+
+        if (finalPrompt.Contains("#{restaurant_info}") || finalPrompt.Contains("#{restaurant_items}"))
+        {
+            var aiKid = await _aiSpeechAssistantDataProvider.GetAiKidAsync(assistant.Id, cancellationToken).ConfigureAwait(false);
+
+            if (aiKid != null)
+            {
+                try
+                {
+                    var response = await _smartiesClient.GetCrmCustomerInfoAsync(aiKid.KidUuid, cancellationToken).ConfigureAwait(false);
+                    
+                    Log.Information("Get crm customer info response: {@Response}", response);
+
+                    var result = SplicingCrmCustomerResponse(response?.Data?.FirstOrDefault());
+
+                    finalPrompt = finalPrompt.Replace("#{restaurant_info}", result.RestaurantInfo).Replace("#{restaurant_items}", result.PurchasedItems);
+                }
+                catch (Exception e)
+                {
+                    Log.Warning("Replace restaurant info failed: {Exception}", e);
+                }
+            }
+        }
+        
+        Log.Information($"The final prompt: {finalPrompt}");
+
+        return finalPrompt;
+    }
+    
+    private (string RestaurantInfo, string PurchasedItems) SplicingCrmCustomerResponse(CrmCustomerInfoDto customerInfo)
+    {
+        var infoSb = new StringBuilder();
+        var itemsSb = new StringBuilder();
+        
+        infoSb.AppendLine($"餐厅名字：{customerInfo.Name}");
+        infoSb.AppendLine($"餐厅地址：{customerInfo.Address}");
+
+        itemsSb.AppendLine("餐厅购买过的items（餐厅所需要的）：");
+        
+        var idx = 1;
+        foreach (var product in customerInfo.Products.OrderByDescending(x => x.CreatedAt))
+        {
+            var itemName = product.Name;
+            var specSb = new StringBuilder();
+            foreach (var attr in product.Attributes)
+            {
+                var attrName = attr.Name;
+                var options = attr.Options;
+                var optionNames = string.Join("、", options.Select(opt => opt.Name.ToString()));
+                specSb.Append($"{attrName}: {optionNames}; ");
+            }
+
+            if (idx < 4)
+                itemsSb.AppendLine($"{idx}. {itemName}(新品)，规格: {specSb.ToString().Trim()}");
+            else
+                itemsSb.AppendLine($"{idx}. {itemName}，规格: {specSb.ToString().Trim()}");
+            
+            idx++;
+        }
+        
+        return (infoSb.ToString(), itemsSb.ToString());
     }
     
     private async Task ReceiveFromWebSocketClientAsync(RealtimeAiEngineContext context, CancellationToken cancellationToken)
