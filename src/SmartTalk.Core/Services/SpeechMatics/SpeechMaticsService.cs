6af3efe8170d740992f308bd341da9fc86f682f8
157 <serena.xiao@sjfood.com>
2025-08-27 11:14:29 +0800

    fix smartalk name

--- src/SmartTalk.Core/Services/SpeechMatics/SpeechMaticsService.cs
+++ src/SmartTalk.Core/Services/SpeechMatics/SpeechMaticsService.cs
@@ -387,7 +387,7 @@ public class SpeechMaticsService : ISpeechMaticsService
         
         var draftOrder = new GenerateAiOrdersRequestDto
         {
-            AiModel = "SmartTalk",
+            AiModel = "Smartalk",
             AiOrderInfoDto = new AiOrderInfoDto
             {
                 SoldToId = aiSpeechAssistant.Name,
