d3599442f811139526e80158c38bdf8b30dd0cd4
TATAN.H <120452548+doudouta@users.noreply.github.com>
2025-09-02 17:44:54 +0800

    fix

--- src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0055_add_pos_service_table.sql
+++ src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0055_add_pos_service_table.sql
@@ -10,26 +10,26 @@ VALUES
     ('SmartTalk', now(3)),
     ('TestOmeNow', now(3));
 
-alter table pos_company modify column service_provider_id int null;
+alter table pos_company add column service_provider_id int null;
 
 alter table pos_company rename to company;
 
 alter table pos_company_store rename to company_store;
 
-alter table pos_store_user rename to stroe_user;
+alter table pos_store_user rename to store_user;
 
-alter table user_account modify column service_provider_id int null;
+alter table user_account add column service_provider_id int null;
 
-alter table agent modify column service_provider_id int null;
+alter table agent add column service_provider_id int null;
 
-alter table role modify column service_provider_id int null;
+alter table role add column service_provider_id int null;
 
-alter table role modify column user_account_level int null;
+alter table role add column user_account_level int null;
 
 INSERT INTO role (created_on, modified_on, uuid, name, user_account_level)
 VALUES
     (now(3), now(3), uuid(), 'TestOperator', 3),
-    (now(3), now(3), uuid(), 'TestAdmin'), 1,
+    (now(3), now(3), uuid(), 'TestAdmin', 1),
     (now(3), now(3), uuid(), 'TestServiceProviderOperator', 1);
 
 INSERT INTO role_permission (created_date, last_modified_date, role_id, permission_id)
