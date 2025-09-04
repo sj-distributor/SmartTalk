b187f79ce92be64763f9c0e716b419fa829774f5
JOHNNY.X <2546088435@qq.com>
2025-09-04 11:06:23 +0800

    update

--- src/SmartTalk.Messages/Commands/Printer/DeleteMerchPrinterCommand.cs
+++ src/SmartTalk.Messages/Commands/Printer/DeleteMerchPrinterCommand.cs
@@ -1,8 +1,11 @@
 using Mediator.Net.Contracts;
+using SmartTalk.Messages.Attributes;
+using SmartTalk.Messages.Constants;
 using SmartTalk.Messages.Responses;
 
 namespace SmartTalk.Messages.Commands.Printer;
 
+[SmartTalkAuthorize(Permissions = new []{ SecurityStore.Permissions.CanViewMerchPrinter})]
 public class DeleteMerchPrinterCommand : ICommand
 {
     public int Id { get; set; }
--- src/SmartTalk.Messages/Requests/Printer/GetMerchPrinterLogRequest.cs
+++ src/SmartTalk.Messages/Requests/Printer/GetMerchPrinterLogRequest.cs
@@ -1,9 +1,12 @@
 using Mediator.Net.Contracts;
+using SmartTalk.Messages.Attributes;
+using SmartTalk.Messages.Constants;
 using SmartTalk.Messages.Enums.Printer;
 using SmartTalk.Messages.Responses;
 
 namespace SmartTalk.Messages.Requests.Printer;
 
+[SmartTalkAuthorize(Permissions = new []{ SecurityStore.Permissions.CanViewMerchPrinter})]
 public class GetMerchPrinterLogRequest : IRequest
 {
     public int StoreId { get; set; }
