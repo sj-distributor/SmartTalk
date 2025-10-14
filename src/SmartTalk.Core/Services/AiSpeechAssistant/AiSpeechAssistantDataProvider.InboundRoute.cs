e1fe44b735d2ae11a032f57a668d61828b3c351e
MonesyH <MonesyH@users.noreply.github.com>
2025-10-14 13:46:21 +0800

    Update AiSpeechAssistantDataProvider.InboundRoute.cs

--- src/SmartTalk.Core/Services/AiSpeechAssistant/AiSpeechAssistantDataProvider.InboundRoute.cs
+++ src/SmartTalk.Core/Services/AiSpeechAssistant/AiSpeechAssistantDataProvider.InboundRoute.cs
@@ -8,8 +8,6 @@ public partial interface IAiSpeechAssistantDataProvider
     Task<List<AiSpeechAssistantInboundRoute>> GetAiSpeechAssistantInboundRoutesAsync(string targetNumber = null, bool? emergency = false, CancellationToken cancellationToken = default);
     
     Task AddAiSpeechAssistantInboundRoutesAsync(List<AiSpeechAssistantInboundRoute> inboundRoutes, bool forceSave = true, CancellationToken cancellationToken = default);
-    
-    Task DeleteAiSpeechAssistantInboundRoutesAsync(List<AiSpeechAssistantInboundRoute> inboundRoutes, bool forceSave = true, CancellationToken cancellationToken = default);
 }
 
 public partial class AiSpeechAssistantDataProvider
@@ -30,13 +28,6 @@ public partial class AiSpeechAssistantDataProvider
     public async Task AddAiSpeechAssistantInboundRoutesAsync(List<AiSpeechAssistantInboundRoute> inboundRoutes, bool forceSave = true, CancellationToken cancellationToken = default)
     {
         await _repository.InsertAllAsync(inboundRoutes, cancellationToken).ConfigureAwait(false);
-        
-        if (forceSave) await _unitOfWork.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-    }
-
-    public async Task DeleteAiSpeechAssistantInboundRoutesAsync(List<AiSpeechAssistantInboundRoute> inboundRoutes, bool forceSave = true, CancellationToken cancellationToken = default)
-    {
-        await _repository.DeleteAllAsync(inboundRoutes, cancellationToken).ConfigureAwait(false);
         
         if (forceSave) await _unitOfWork.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
     }
