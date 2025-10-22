01c5dd7b4ca5698cdf5ea21482803614662203f6
FREYA.F <freya.fang@sjfood.com>
2025-10-22 11:08:52 +0800

    update codes

--- src/SmartTalk.Messages/Requests/HrInterView/GetHrInterViewSessionsRequest.cs
+++ src/SmartTalk.Messages/Requests/HrInterView/GetHrInterViewSessionsRequest.cs
@@ -10,7 +10,7 @@ public class GetHrInterViewSessionsRequest : IRequest
 
     public int? PageIndex { get; set; }
 
-    public int? PageSzie { get; set; }
+    public int? PageSize { get; set; }
 }
 
 public class GetHrInterViewSessionsResponse : SmartTalkResponse
