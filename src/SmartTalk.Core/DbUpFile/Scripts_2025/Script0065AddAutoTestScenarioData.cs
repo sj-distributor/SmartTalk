727843569be5598cf0c556d142c3d34d050f83f1
FREYA.F <freya.fang@sjfood.com>
2025-10-31 11:19:20 +0800

    update cods

--- src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0065AddAutoTestScenarioData.cs
+++ src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0065AddAutoTestScenarioData.cs
@@ -13,7 +13,20 @@ public class Script0065AddAutoTestScenarioData : IScript
         stringBuilder.AppendLine($@"
             INSERT INTO auto_test_scenario ( key_name, name,  input_schema, output_schema,  action_config, action_type, created_at) 
             VALUES ('AiOrder', 'AiOrder', 
-                    '{{ ""recording"": {{ ""type"": ""string"", ""required"": true, ""desc"": ""录音信息（如URL或标识）"" }},""orderId"": {{ ""type"": ""string"", ""required"": true, ""desc"": ""订单唯一编号"" }}}}',
+                    '{{ ""recording"": {{ ""type"": ""string"", ""required"": true, ""desc"": ""录音信息（如URL或标识）"" }},""orderId"": {{ ""type"": ""string"", ""required"": true, ""desc"": ""订单唯一编号"" }},
+                    ""detail"": {{
+                        ""type"": ""array"",
+                        ""required"": true,
+                        ""items"": {{
+                            ""type"": ""object"",
+                            ""properties"": {{
+                                ""SerialNumber"": {{ ""type"": ""number"", ""desc"": ""序号"" }},
+                                ""Quantity"": {{ ""type"": ""integer"", ""desc"": ""数量"" }},
+                                ""ItemDesc"": {{ ""type"": ""number"", ""desc"": ""内容"" }}
+                            }}
+                        }},
+                        ""desc"": ""订单明细列表""
+                    }}}}',
                     '{{ }}', '{{ }}',  0,  NOW()); ");
         
         return stringBuilder.ToString();
