234d818c8371afc9a9abf95e563d89456d7adb15
157 <serena.xiao@sjfood.com>
2025-11-07 18:03:47 +0800

    fixed

--- src/SmartTalk.Core/Settings/Crm/CrmSetting.cs
+++ src/SmartTalk.Core/Settings/Crm/CrmSetting.cs
@@ -4,6 +4,15 @@ namespace SmartTalk.Core.Settings.Crm;
 
 public class CrmSetting : IConfigurationSetting
 {
+    public CrmSetting(IConfiguration configuration)
+    {
+        BaseUrl = configuration.GetValue<string>("Crm:BaseUrl");
+        
+        ClientId = configuration.GetValue<string>("Crm:ClientId");
+        
+        ClientSecret = configuration.GetValue<string>("Crm:ClientSecret");
+    }
+
     public string BaseUrl { get; set; }
     
     public string ClientId { get; set; }
