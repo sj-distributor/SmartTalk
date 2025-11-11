23bb352a088fa0c72f84d6e6707166294191d4ad
157 <serena.xiao@sjfood.com>
2025-11-12 07:22:06 +0800

    Update RingCentralTokenResponseDto.cs

--- src/SmartTalk.Messages/Dto/RingCentral/RingCentralTokenResponseDto.cs
+++ src/SmartTalk.Messages/Dto/RingCentral/RingCentralTokenResponseDto.cs
@@ -1,24 +1,36 @@
+using System.Text.Json.Serialization;
+
 namespace SmartTalk.Messages.Dto.RingCentral;
 
 public class RingCentralTokenResponseDto
 {
+    [JsonPropertyName("access_token")]
     public string AccessToken { get; set; }
     
+    [JsonPropertyName("token_type")]
     public string TokenType { get; set; }
     
+    [JsonPropertyName("expires_in")]
     public int ExpiresIn { get; set; }
     
+    [JsonPropertyName("refresh_token")]
     public string RefreshToken { get; set; }
     
+    [JsonPropertyName("refresh_token_expires_in")]
     public int RefreshTokenExpiresIn { get; set; }
     
+    [JsonPropertyName("scope")]
     public string Scope { get; set; }
     
+    [JsonPropertyName("owner_id")]
     public string OwnerId { get; set; }
     
+    [JsonPropertyName("endpoint_id")]
     public string EndpointId { get; set; }
     
+    [JsonPropertyName("session_id")]
     public string SessionId { get; set; }
     
+    [JsonPropertyName("session_idle_timeout")]
     public int SessionIdleTimeout { get; set; }
 }
