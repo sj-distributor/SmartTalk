69a9e6cbc4c3e312862107028c5e5d8c4c7c0841
157 <serena.xiao@sjfood.com>
2025-11-14 17:59:27 +0800

    Delete Script0069_add_customer_items_cache_table.sql

--- src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0069_add_customer_items_cache_table.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists `customer_items_cache`
-(
-    `id` int auto_increment PRIMARY KEY,
-    `cache_key` varchar(255) NOT NULL,
-    `cache_value` longtext NOT NULL,
-    `last_updated` datetime(3) NOT NULL
-    ) charset = utf8mb4;
-
-CREATE INDEX `idx_cache_key` ON `customer_items_cache` (cache_key);
