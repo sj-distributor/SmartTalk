493e9e54b6d2d918240b0814a502211ecdb069e9
157 <serena.xiao@sjfood.com>
2025-11-14 17:44:33 +0800

    Update Script0068_add_record_id_for_phone_order_record.sql

--- src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0068_add_record_id_for_phone_order_record.sql
+++ src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0068_add_record_id_for_phone_order_record.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `phone_order_record` ADD COLUMN `order_id` VARCHAR(1024) NULL;
+ALTER TABLE `phone_order_record` MODIFY COLUMN `order_id` VARCHAR(1024) NULL;
 ALTER TABLE `phone_order_record` ADD COLUMN `conversation_text` TEXT NULL;
