eed9b8c2fbd927399f6aedc8b8fb5240cae341c3
157 <serena.xiao@sjfood.com>
2025-11-19 14:37:38 +0800

    format

--- src/SmartTalk.Core/Domain/Sales/CustomerItemsCache.cs
+++ src/SmartTalk.Core/Domain/Sales/CustomerItemsCache.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace SmartTalk.Core.Domain.Sales;
 
 [Table("customer_items_cache")]
-public class CustomerItemsCache: IEntity
+public class CustomerItemsCache : IEntity
 {
     [Key]
     [Column("id")]
