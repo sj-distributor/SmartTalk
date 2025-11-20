0ec07879c53a46ffb3d99ccbc806b8063db45e5b
157 <serena.xiao@sjfood.com>
2025-11-20 11:37:04 +0800

    Update CrmTokenResponse.cs

--- src/SmartTalk.Messages/Dto/Crm/CrmTokenResponse.cs
+++ src/SmartTalk.Messages/Dto/Crm/CrmTokenResponse.cs
@@ -1,6 +1,9 @@
+using Newtonsoft.Json;
+
 namespace SmartTalk.Messages.Dto.Crm;
 
 public class CrmTokenResponse
 {
+    [JsonProperty("access_token")]
     public string AccessToken { get; set; }
 }
