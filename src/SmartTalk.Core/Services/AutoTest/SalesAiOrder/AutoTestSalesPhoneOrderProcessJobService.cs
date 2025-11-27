1aad525c95f7e3ef5ab47609a49fac79539daef5
157 <serena.xiao@sjfood.com>
2025-11-27 15:22:03 +0800

    Revert

--- src/SmartTalk.Core/Services/AutoTest/SalesAiOrder/AutoTestSalesPhoneOrderProcessJobService.cs
+++ src/SmartTalk.Core/Services/AutoTest/SalesAiOrder/AutoTestSalesPhoneOrderProcessJobService.cs
@@ -972,10 +972,6 @@ public class AutoTestSalesPhoneOrderProcessJobService : IAutoTestSalesPhoneOrder
             }
         }, cancellationToken).ConfigureAwait(false);
 
-        var ossUrl = ossResponse.Attachment?.FileUrl ?? throw new InvalidOperationException("上传 OSS 失败");
-        
-        var cleanUrl = ossUrl.Split('?')[0];
-
-        return cleanUrl;
+        return ossResponse.Attachment?.FileUrl ?? throw new InvalidOperationException("上传 OSS 失败");
     }
 }
