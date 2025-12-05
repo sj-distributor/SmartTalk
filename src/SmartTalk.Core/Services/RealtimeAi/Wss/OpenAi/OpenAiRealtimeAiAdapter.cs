c099dbb167987abc225ab21f8eec6bbc96b912be
MonesyH <MonesyH@users.noreply.github.com>
2025-12-05 17:04:37 +0800

    Update OpenAiRealtimeAiAdapter.cs

--- src/SmartTalk.Core/Services/RealtimeAi/Wss/OpenAi/OpenAiRealtimeAiAdapter.cs
+++ src/SmartTalk.Core/Services/RealtimeAi/Wss/OpenAi/OpenAiRealtimeAiAdapter.cs
@@ -7,6 +7,7 @@ using SmartTalk.Core.Services.RealtimeAi.Wss;
 using SmartTalk.Core.Settings.OpenAi;
 using SmartTalk.Messages.Dto.RealtimeAi;
 using SmartTalk.Messages.Enums.AiSpeechAssistant;
+using SmartTalk.Messages.Enums.Hr;
 using SmartTalk.Messages.Enums.RealtimeAi;
 using JsonException = System.Text.Json.JsonException;
 using JsonSerializer = System.Text.Json.JsonSerializer;
@@ -40,6 +41,22 @@ public class OpenAiRealtimeAiAdapter : IRealtimeAiProviderAdapter
     {
         var configs = await InitialSessionConfigAsync(assistantProfile, cancellationToken).ConfigureAwait(false);
         var knowledge = await _aiSpeechAssistantDataProvider.GetAiSpeechAssistantKnowledgeAsync(assistantProfile.Id, isActive: true, cancellationToken: cancellationToken).ConfigureAwait(false);
+        
+        if (knowledge.Prompt.Contains("#{hr_interview_section1}", StringComparison.OrdinalIgnoreCase))
+        {
+            var cacheKeys = Enum.GetValues(typeof(HrInterviewQuestionSection))
+                .Cast<HrInterviewQuestionSection>()
+                .Select(section => "hr_interview_" + section.ToString().ToLower())
+                .ToList();
+
+            var caches = await _aiSpeechAssistantDataProvider.GetAiSpeechAssistantKnowledgeVariableCachesAsync(cacheKeys, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            foreach (var section in Enum.GetValues(typeof(HrInterviewQuestionSection)).Cast<HrInterviewQuestionSection>())
+            {
+                var cacheKey = $"hr_interview_{section.ToString().ToLower()}";
+                knowledge.Prompt = knowledge.Prompt.Replace($"#{cacheKey}", caches.FirstOrDefault(x => x.CacheKey == cacheKey)?.CacheValue);
+            }
+        }
         
         var sessionPayload = new
         {
