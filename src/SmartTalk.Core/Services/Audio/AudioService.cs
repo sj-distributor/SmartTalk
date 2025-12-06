45ec3614ebf19e75dec71fc1eb7eeb90c6c0dbb1
zirui liu <maxon.liu@sjfood.com>
2025-12-06 14:44:27 +0800

    enhance

--- src/SmartTalk.Core/Services/Audio/AudioService.cs
+++ src/SmartTalk.Core/Services/Audio/AudioService.cs
@@ -41,9 +41,9 @@ public class AudioService : IAudioService
         };
     }
 
-    private async Task<AudioData> GetAudioBinaryDataAsync(AnalyzeAudioCommand command, CancellationToken cancellationToken)
+    private async Task<BinaryData> GetAudioBinaryDataAsync(AnalyzeAudioCommand command, CancellationToken cancellationToken)
     {
-        var audioData = new AudioData();
+        BinaryData audioData;
 
         if (!string.IsNullOrWhiteSpace(command.AudioUrl))
         {
@@ -51,36 +51,16 @@ public class AudioService : IAudioService
 
             await using var stream = await httpClient.GetStreamAsync(command.AudioUrl, cancellationToken).ConfigureAwait(false);
 
-            audioData.Type = AudioDataType.Binary;
-            audioData.BinaryContent = await BinaryData.FromStreamAsync(stream, cancellationToken);
-        }
-        else if (command.AudioContent is not null && command.AudioContent.Length != 0)
-        {
-
-            audioData.Type = AudioDataType.Binary;
-            audioData.BinaryContent = BinaryData.FromBytes(command.AudioContent);
+            audioData = await BinaryData.FromStreamAsync(stream, cancellationToken);
         }
         else
         {
-            audioData.Type = AudioDataType.Base64;
-            audioData.Base64String = command.AudioBase64;
+            if (command.AudioContent is null || command.AudioContent.Length == 0)
+                throw new InvalidOperationException("Audio content is empty.");
+
+            audioData = BinaryData.FromBytes(command.AudioContent);
         }
 
         return audioData;
-    }
-
-    public class AudioData
-    {
-        public AudioDataType Type { get; set; }
-        
-        public BinaryData BinaryContent { get; set; }
-        
-        public string Base64String { get; set; }
-    }
-
-    public enum AudioDataType
-    {
-        Binary,
-        Base64
     }
 }
