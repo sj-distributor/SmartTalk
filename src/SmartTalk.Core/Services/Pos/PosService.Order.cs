bf00e12bbd7dc16ee529cf267595be6c5537e9a8
Travis.C <76553526+happyshower@users.noreply.github.com>
2025-12-16 16:31:37 +0800

    Update PosService.Order.cs

--- src/SmartTalk.Core/Services/Pos/PosService.Order.cs
+++ src/SmartTalk.Core/Services/Pos/PosService.Order.cs
@@ -657,6 +657,10 @@ public partial class PosService
 
                     if (isWithSpecifications && product.Item2.Count > 0)
                     {
+                        var matchedModifiers = simpleModifiers.FirstOrDefault(x => x.ProductId == item.ProductId.ToString());
+
+                        if (matchedModifiers != null) continue;
+                        
                         simpleModifiers.Add(new PosProductSimpleModifiersDto
                         {
                             ProductId = item.ProductId.ToString(),
