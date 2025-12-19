808549fa5112e9d228baf137e71591fa8f7c53fc
Johnny <145190666+xieyangp@users.noreply.github.com>
2025-12-20 02:31:34 +0800

    update

---
+++ src/SmartTalk.Core/Handlers/CommandHandlers/Pos/RetryCloudPrintingCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Mediator.Net.Context;
+using Mediator.Net.Contracts;
+using SmartTalk.Core.Services.Pos;
+using SmartTalk.Messages.Commands.Pos;
+
+namespace SmartTalk.Core.Handlers.CommandHandlers.Pos;
+
+public class RetryCloudPrintingCommandHandler : ICommandHandler<RetryCloudPrintingCommand>
+{
+    private readonly IPosService _posService;
+
+    public RetryCloudPrintingCommandHandler(IPosService posService)
+    {
+        _posService = posService;
+    }
+    
+    public async Task Handle(IReceiveContext<RetryCloudPrintingCommand> context, CancellationToken cancellationToken)
+    {
+        await _posService.RetryCloudPrintingAsync(context.Message, cancellationToken).ConfigureAwait(false);
+    }
+}
