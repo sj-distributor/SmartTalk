d480fd17983da847b72639a9989dad6ae89c624c
LIZZIE.L <lyidi444@gmail.com>
2026-01-21 10:05:41 +0800

    Update task notification

---
+++ src/SmartTalk.Messages/Commands/Security/UpdateUserAccountTaskNotificationCommand.cs
@@ -0,0 +1,23 @@
+using Mediator.Net.Contracts;
+using SmartTalk.Messages.Dto.Account;
+using SmartTalk.Messages.Responses;
+
+namespace SmartTalk.Messages.Commands.Security;
+
+public class UpdateUserAccountTaskNotificationCommand : ICommand
+{
+    public int UserId { get; set; }
+    
+    public bool? IsTaskEnabled { get; set; }
+
+    public bool? IsTurnOnNotification { get; set; }
+}
+
+public class UpdateUserAccountTaskNotificationResponse: SmartTalkResponse<UpdateUserAccountTaskNotificationResponseData>
+{
+}
+
+public class UpdateUserAccountTaskNotificationResponseData
+{
+    public UserAccountDto UserAccount {get; set;}
+}
