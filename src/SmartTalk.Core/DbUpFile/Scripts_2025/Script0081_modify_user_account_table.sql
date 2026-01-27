f00985850f280c9c515e47dbed9370196bb2553e
JOHNNY.X <2546088435@qq.com>
2026-01-27 20:54:48 +0800

    Delete Script0081_modify_user_account_table.sql

--- src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0081_modify_user_account_table.sql
+++
@@ -1,2 +0,0 @@
-alter table `user_account` add column `is_task_enabled` tinyint(1) not null default 0;
-alter table `user_account` add column `is_turn_on_notification` tinyint(1) not null default 0;
