cc991def45b6ece4d8d75a97349733104a7d2b63
LIZZIE.L <lyidi444@gmail.com>
2026-02-12 15:38:31 +0800

    add recurring job

--- src/SmartTalk.Core/Jobs/RecurringJobs/SchedulingSyncAiSpeechAssistantKnowledgePromptRecurringJob.cs
+++ src/SmartTalk.Core/Jobs/RecurringJobs/SchedulingSyncAiSpeechAssistantKnowledgePromptRecurringJob.cs
@@ -1,4 +1,3 @@
-using Hangfire;
 using Mediator.Net;
 using SmartTalk.Core.Settings.Jobs;
 using SmartTalk.Messages.Commands.AiSpeechAssistant;
@@ -8,10 +7,14 @@ namespace SmartTalk.Core.Jobs.RecurringJobs;
 public class SchedulingSyncAiSpeechAssistantKnowledgePromptRecurringJob : IRecurringJob
 {
     private readonly IMediator _mediator;
+    private readonly SchedulingSyncAiSpeechAssistantKnowledgePromptRecurringJobExpressionSetting _settings;
 
-    public SchedulingSyncAiSpeechAssistantKnowledgePromptRecurringJob(IMediator mediator)
+    public SchedulingSyncAiSpeechAssistantKnowledgePromptRecurringJob(
+        IMediator mediator,
+        SchedulingSyncAiSpeechAssistantKnowledgePromptRecurringJobExpressionSetting settings)
     {
         _mediator = mediator;
+        _settings = settings;
     }
 
     public async Task Execute()
@@ -21,6 +24,5 @@ public class SchedulingSyncAiSpeechAssistantKnowledgePromptRecurringJob : IRecur
 
     public string JobId => nameof(SchedulingSyncAiSpeechAssistantKnowledgePromptRecurringJob);
 
-    public string CronExpression => "*/5 * * * *";
+    public string CronExpression => _settings.Value;
 }
-
