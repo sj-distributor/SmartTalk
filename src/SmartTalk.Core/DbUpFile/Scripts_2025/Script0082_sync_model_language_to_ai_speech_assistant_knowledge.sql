b1da03475753d57ae799f3bccfd53dfcd131dffc
LIZZIE.L <lyidi444@gmail.com>
2026-02-12 10:38:57 +0800

    fix sql

--- src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0082_sync_model_language_to_ai_speech_assistant_knowledge.sql
+++ src/SmartTalk.Core/DbUpFile/Scripts_2025/Script0082_sync_model_language_to_ai_speech_assistant_knowledge.sql
@@ -1,5 +1,4 @@
 UPDATE `ai_speech_assistant_knowledge` knowledge
     JOIN `ai_speech_assistant` assistant ON assistant.`id` = knowledge.`assistant_id`
     SET knowledge.`model_language` = assistant.`model_language`
-WHERE assistant.`model_language` IS NOT NULL
-  AND knowledge.`is_active` = 1;
+WHERE assistant.`model_language` IS NOT NULL;
