33e413f98e5885d4316e45c5b3f3977866d639b7
Mars.P <marsxxd@outlook.com>
2026-02-17 09:02:13 +0800

    refactoring function calls

---
+++ src/SmartTalk.Core/Services/AiSpeechAssistantConnect/AiSpeechAssistantConnectService.FunctionCalls.Confirm.cs
@@ -0,0 +1,41 @@
+using Newtonsoft.Json;
+using SmartTalk.Messages.Dto.AiSpeechAssistant;
+using SmartTalk.Messages.Dto.RealtimeAi;
+
+namespace SmartTalk.Core.Services.AiSpeechAssistantConnect;
+
+public partial class AiSpeechAssistantConnectService
+{
+    private RealtimeAiFunctionCallResult ProcessConfirmOrder(RealtimeAiWssFunctionCallData functionCallData)
+    {
+        _ctx.OrderItems = JsonConvert.DeserializeObject<AiSpeechAssistantOrderDto>(functionCallData.ArgumentsJson);
+
+        return new RealtimeAiFunctionCallResult
+        {
+            Output = "Please confirm the order content with the customer. If this is the first time confirming, repeat the order details. Once the customer confirms, do not repeat the details again. " +
+                     "If the order is confirmed, we will proceed with asking for the pickup time and will no longer repeat the order details."
+        };
+    }
+
+    private RealtimeAiFunctionCallResult ProcessConfirmCustomerInformation(RealtimeAiWssFunctionCallData functionCallData)
+    {
+        _ctx.UserInfo = JsonConvert.DeserializeObject<AiSpeechAssistantUserInfoDto>(functionCallData.ArgumentsJson);
+
+        return new RealtimeAiFunctionCallResult
+        {
+            Output = "Reply in the guest's language: OK, I've recorded it for you."
+        };
+    }
+
+    private RealtimeAiFunctionCallResult ProcessConfirmPickupTime(RealtimeAiWssFunctionCallData functionCallData)
+    {
+        var parsed = JsonConvert.DeserializeObject<AiSpeechAssistantOrderDto>(functionCallData.ArgumentsJson);
+        if (_ctx.OrderItems != null)
+            _ctx.OrderItems.Comments = parsed?.Comments ?? string.Empty;
+
+        return new RealtimeAiFunctionCallResult
+        {
+            Output = "Record the time when the customer pickup the order."
+        };
+    }
+}
