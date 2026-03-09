c23bb95926e48272f2d33143ba70578eae78d3d9
TATAN.H <120452548+doudouta@users.noreply.github.com>
2026-03-09 11:06:36 +0800

    Update Script0002_add_ai_speech_assistant_knowledge_detail_table.sql

--- src/SmartTalk.Core/DbUpFile/Scripts_2026/Script0002_add_ai_speech_assistant_knowledge_detail_table.sql
+++ src/SmartTalk.Core/DbUpFile/Scripts_2026/Script0002_add_ai_speech_assistant_knowledge_detail_table.sql
@@ -10,38 +10,3 @@ CREATE TABLE IF NOT EXISTS ai_speech_assistant_knowledge_detail (
   PRIMARY KEY (`id`),
   INDEX `idx_knowledge_id` (`knowledge_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO ai_speech_assistant_knowledge_detail
-(
-    knowledge_id,
-    knowledge_name,
-    format_type,
-    content,
-    created_date,
-    last_modified_date
-)
-SELECT
-    k.id,
-    j.key_name,
-    0,
-    jt.value,
-    NOW(),
-    NOW()
-FROM ai_speech_assistant_knowledge k
-         JOIN JSON_TABLE(
-        JSON_KEYS(k.json),
-        '$[*]' COLUMNS (
-        key_name VARCHAR(255) PATH '$'
-    )
-              ) j
-         JOIN JSON_TABLE(
-        JSON_EXTRACT(
-            k.json,
-            CONCAT('$."', REPLACE(j.key_name, '"', '\\"'), '"')
-        ),
-        '$[*]' COLUMNS (
-        value TEXT PATH '$'
-    )
-              ) jt
-WHERE k.json IS NOT NULL
-  AND k.json <> '{}';
