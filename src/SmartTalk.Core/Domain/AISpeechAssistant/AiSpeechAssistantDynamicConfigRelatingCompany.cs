1122af94c678fee3553cc382456c783fad7c0192
TATAN.H <120452548+doudouta@users.noreply.github.com>
2026-03-11 14:34:49 +0800

    fix AiSpeechAssistantDynamicConfigRelatingCompany table

--- src/SmartTalk.Core/Domain/AISpeechAssistant/AiSpeechAssistantDynamicConfigRelatingCompany.cs
+++ src/SmartTalk.Core/Domain/AISpeechAssistant/AiSpeechAssistantDynamicConfigRelatingCompany.cs
@@ -1,12 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SmartTalk.Core.Domain.AISpeechAssistant;
 
+[Table("ai_speech_assistant_dynamic_config_relating_company")]
 public class AiSpeechAssistantDynamicConfigRelatingCompany : IEntity
 {
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     
+    [Column("config_id")]
     public int ConfigId { get; set; }
     
+    [Column("company_id")]
     public int CompanyId { get; set; }
     
+    [Column("company_name")]
     public string CompanyName { get; set; }
 }
