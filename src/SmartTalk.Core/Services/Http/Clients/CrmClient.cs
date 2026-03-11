5ababb465407bd0650eecf3f84e9beab07ed17c9
157 <serena.xiao@sjfood.com>
2026-03-11 18:44:04 +0800

    Update CrmClient.cs

--- src/SmartTalk.Core/Services/Http/Clients/CrmClient.cs
+++ src/SmartTalk.Core/Services/Http/Clients/CrmClient.cs
@@ -119,7 +119,7 @@ public class CrmClient : ICrmClient
         return await _httpClient.GetAsync<List<CrmContactDto>>($"{_crmSetting.BaseUrl}/api/customer/{customerId}/contacts", headers: headers, cancellationToken: cancellationToken).ConfigureAwait(false);
     }
 
-    public async Task<List<GetDeliveryInfoByPhoneNumberResponseDto>> GetDeliveryInfoByPhoneNumberAsync(string phoneNumber, string apiKey = null, CancellationToken cancellationToken = default)
+    public async Task<List<GetDeliveryInfoByPhoneNumberResponseDto>> GetDeliveryInfoByPhoneNumberAsync(string phoneNumber, CancellationToken cancellationToken = default)
     {
         if (string.IsNullOrWhiteSpace(phoneNumber))
             throw new ArgumentException("phoneNumber cannot be null or empty.", nameof(phoneNumber));
@@ -128,7 +128,7 @@ public class CrmClient : ICrmClient
 
         var headers = new Dictionary<string, string>
         {
-            { "X-API-KEY", apiKey },
+            { "X-API-KEY", _crmSetting.ApiKey },
             { "Accept", "application/json" }
         };
 
