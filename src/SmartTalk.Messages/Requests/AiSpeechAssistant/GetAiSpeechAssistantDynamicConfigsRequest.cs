15facb34bc5c2c182e723d366bf1eaa648d885d5
TATAN.H <120452548+doudouta@users.noreply.github.com>
2026-03-13 15:39:28 +0800

    add CanViewDynamicConfig

--- src/SmartTalk.Messages/Requests/AiSpeechAssistant/GetAiSpeechAssistantDynamicConfigsRequest.cs
+++ src/SmartTalk.Messages/Requests/AiSpeechAssistant/GetAiSpeechAssistantDynamicConfigsRequest.cs
@@ -1,10 +1,13 @@
 using Mediator.Net.Contracts;
+using SmartTalk.Messages.Attributes;
+using SmartTalk.Messages.Constants;
 using SmartTalk.Messages.Dto.AiSpeechAssistant;
 using SmartTalk.Messages.Dto.Pos;
 using SmartTalk.Messages.Responses;
 
 namespace SmartTalk.Messages.Requests.AiSpeechAssistant;
 
+[SmartTalkAuthorize(Permissions = new[] { SecurityStore.Permissions.CanViewDynamicConfig })]
 public class GetAiSpeechAssistantDynamicConfigsRequest : IRequest
 {
 }
