43978d9f0471efc588146a7f0e9d6147cc13cb2b
LIZZIE.L <lyidi444@gmail.com>
2026-04-23 14:57:56 +0800

    delete index

--- src/SmartTalk.Core/DbUpFile/Scripts_2026/Script0007_add_knowledge_scene_tables.sql
+++ src/SmartTalk.Core/DbUpFile/Scripts_2026/Script0007_add_knowledge_scene_tables.sql
@@ -36,17 +36,3 @@ create table if not exists ai_speech_assistant_knowledge_scene_relation
     `scene_id` int not null,
     `created_at` datetime(3) not null
     ) charset=utf8mb4;
-
-create index idx_knowledge_scene_folder_updated_at on knowledge_scene_folder (`updated_at`);
-create index idx_knowledge_scene_folder_created_at on knowledge_scene_folder (`created_at`);
-
-create index idx_knowledge_scene_folder_id on knowledge_scene (`folder_id`);
-create index idx_knowledge_scene_updated_at on knowledge_scene (`updated_at`);
-create index idx_knowledge_scene_created_at on knowledge_scene (`created_at`);
-
-create index idx_knowledge_scene_knowledge_scene_id on knowledge_scene_knowledge (`scene_id`);
-create index idx_knowledge_scene_knowledge_updated_at on knowledge_scene_knowledge (`updated_at`);
-create index idx_knowledge_scene_knowledge_created_at on knowledge_scene_knowledge (`created_at`);
-
-create index idx_ai_speech_assistant_knowledge_scene_relation_knowledge_id on ai_speech_assistant_knowledge_scene_relation (`knowledge_id`);
-create index idx_ai_speech_assistant_knowledge_scene_relation_scene_id on ai_speech_assistant_knowledge_scene_relation (`scene_id`);
