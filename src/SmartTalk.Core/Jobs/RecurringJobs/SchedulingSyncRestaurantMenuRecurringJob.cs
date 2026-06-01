3b5a8b93d6b8bb7726f138e15f18727c448eedc7
157 <serena.xiao@sjfood.com>
2026-06-01 15:23:45 +0800

    Restore mixture_v6 recurring job interface

--- src/SmartTalk.Core/Jobs/RecurringJobs/SchedulingSyncRestaurantMenuRecurringJob.cs
+++ src/SmartTalk.Core/Jobs/RecurringJobs/SchedulingSyncRestaurantMenuRecurringJob.cs
@@ -4,7 +4,7 @@ using SmartTalk.Messages.Commands.Restaurant;
 
 namespace SmartTalk.Core.Jobs.RecurringJobs;
 
-public class SchedulingSyncRestaurantMenuRecurringJob
+public class SchedulingSyncRestaurantMenuRecurringJob : IRecurringJob
 {
     private readonly IMediator _mediator;
 
