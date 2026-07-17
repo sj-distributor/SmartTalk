808ecd3cd24bea318569905c9448183411ef15b6
LIZZIE.L <lyidi444@gmail.com>
2026-07-17 14:46:26 +0800

    delete useless code

--- src/SmartTalk.Core/DbUpFile/Scripts_2026/Script0020_add_scene_source_columns_to_ai_speech_assistant_knowledge_detail.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ai_speech_assistant_knowledge_detail
-    ADD COLUMN `source_type` VARCHAR(32) NULL,
-    ADD COLUMN `source_scene_id` INT NULL,
-    ADD COLUMN `source_scene_item_id` INT NULL;
