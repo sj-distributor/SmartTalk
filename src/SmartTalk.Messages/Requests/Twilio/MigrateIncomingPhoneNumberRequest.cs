d20579552d19c70343b1f8f5cb292f20336dff42
LIZZIE.L <lyidi444@gmail.com>
2026-08-04 16:31:55 +0800

    Revert "delete code"
    This reverts commit 4a6fb6ea8cb111d010af1f4ad19bfe29fa71a7ee.

---
+++ src/SmartTalk.Messages/Requests/Twilio/MigrateIncomingPhoneNumberRequest.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.DataAnnotations;
+using Mediator.Net.Contracts;
+using SmartTalk.Messages.Responses;
+
+namespace SmartTalk.Messages.Requests.Twilio;
+
+public class MigrateIncomingPhoneNumberRequest : IRequest
+{
+    [Required]
+    public string PhoneNumberSid { get; set; }
+
+    [Required]
+    public string LosingAccountSid { get; set; }
+
+    [Required]
+    public string LosingAccountAuthToken { get; set; }
+    
+    [Required]
+    public string GainingAccountSid { get; set; }
+
+    public string BundleSid { get; set; }
+
+    public string AddressSid { get; set; }
+}
+
+public class MigrateIncomingPhoneNumberResponse : SmartTalkResponse
+{
+    public string Sid { get; set; }
+
+    public string AccountSid { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string FriendlyName { get; set; }
+
+    public string BundleSid { get; set; }
+
+    public string AddressSid { get; set; }
+
+    public DateTimeOffset? DateUpdated { get; set; }
+
+    public int? Code { get; set; }
+
+    public string Message { get; set; }
+
+    public string MoreInfo { get; set; }
+
+    public int? Status { get; set; }
+}
